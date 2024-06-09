f63b136aec6e3b6e1e1c3a25d86566b6d60bfa4d
lesanabbas <lesanabbas@gmail.com>
2024-06-09 18:00:36 +0530

    build file changes for vercel

--- build_files.sh
+++ build_files.sh
@@ -5,16 +5,16 @@
 
 echo "Building the project..."
 
-python3.11 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 echo "Make Migrations"
 
-python3.11 manage.py makemigrations --noinput
+python3 manage.py makemigrations --noinput
 
-python3.11 manage.py migrate --noinput
+python3 manage.py migrate --noinput
 
 echo "Collect Static"
 
-python3.11 manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
